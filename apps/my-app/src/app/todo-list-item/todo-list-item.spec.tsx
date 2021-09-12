3d4a604f47bf5d9d9a963c3f85c61a4ab0e0e806
alex860111 <alex86011119@googlemail.com>
2021-09-12 20:17:41 +0200

    Added test

--- apps/my-app/src/app/todo-list-item/todo-list-item.spec.tsx
+++ apps/my-app/src/app/todo-list-item/todo-list-item.spec.tsx
@@ -3,15 +3,19 @@ import { Todo } from '../todo/todo';
 import { TodoListItem } from './todo-list-item';
 
 describe(TodoListItem, () => {
+  let todo: Todo;
+
+  beforeEach(() => {
+    todo = { id: 1, content: 'hello world' };
+  });
+
   it('should render successfully', () => {
-    const todo: Todo = { id: 1, content: 'hello world' };
     const { baseElement } = render(<TodoListItem todo={todo} />);
 
     expect(baseElement).toBeTruthy();
   });
 
   it('should render the todo content as a list item', () => {
-    const todo: Todo = { id: 1, content: 'hello world' };
     const { getByText } = render(<TodoListItem todo={todo} />);
 
     expect(getByText(todo.content).tagName).toEqual('LI');
---
+++ apps/my-app/src/app/todo-list/todo-list.spec.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { Todo } from '../todo/todo';
+import { TodoList } from './todo-list';
+
+jest.mock('../todo-list-item/todo-list-item', () => ({
+  TodoListItem: () => <li data-testid="todo-list-item"></li>,
+}));
+
+describe(TodoList, () => {
+  let todoList: Todo[];
+
+  beforeEach(() => {
+    const todo: Todo = { id: 1, content: 'hello world' };
+    todoList = [todo];
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(<TodoList todoList={todoList} />);
+
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should render the list items', () => {
+    const { getAllByTestId } = render(<TodoList todoList={todoList} />);
+
+    expect(getAllByTestId('todo-list-item').length).toEqual(1);
+  });
+});

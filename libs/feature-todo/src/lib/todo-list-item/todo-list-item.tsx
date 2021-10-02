e1319ff7e6e274e7deaf6b9ce89174da1f79e1ba
alex860111 <alex86011119@googlemail.com>
2021-10-02 17:55:47 +0200

    render delete button

--- libs/feature-todo/src/lib/todo-list-item/todo-list-item.tsx
+++ libs/feature-todo/src/lib/todo-list-item/todo-list-item.tsx
@@ -10,6 +10,15 @@ interface State {}
 
 export class TodoListItem extends React.Component<Props, State> {
   render() {
-    return <li className="list-group-item">{this.props.todo.content}</li>;
+    return (
+      <li className="list-group-item">
+        <div className="d-flex justify-content-between align-items-center">
+          <span>{this.props.todo.content}</span>
+          <button type="button" className="btn btn-danger btn-sm">
+            X
+          </button>
+        </div>
+      </li>
+    );
   }
 }
